12e12533a87747c90e96c1aecbc955bef674fb41
Bill Horst <bill@billhorst.com>
2018-05-13 10:28:26 -0600

    added orders mvc

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @order_product = "Sup?"
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "console-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+Product.delete_all
+Order.delete_all
+
+Product.create(
+  [
+    {
+      name: "Super Fast Bike",
+      description: "A fast bike!",
+      image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=",
+      color: "red",
+      price: 10
+    },
+    {
+      name: "Super Cool Bike",
+      description: "A cool bike!",
+      image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=",
+      color: "blue",
+      price: 20
+    },
+    {
+      name: "Super Green Bike",
+      description: "A green bike!",
+      image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=",
+      color: "purple",
+      price: 103
+    }
+  ]
+)
+
+Order.create(
+  user_id: 1,
+  product_id: 13,
+  total: 20
+)
+# t.integer "user_id"
+# t.integer "product_id"
+# t.float "total"
+# t.index ["product_id"], name: "index_orders_on_product_id"
+# t.index ["user_id"], name: "index_orders_on_user_id"
