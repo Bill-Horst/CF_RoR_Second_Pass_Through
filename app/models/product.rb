1a4cef1ce7513f25baad3db094c93a77de792c2c
Bill Horst <bill@billhorst.com>
2018-05-16 20:32:59 -0600

    forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  def self.search(search_term)
+    if Rails.env.development?
+      @products = Product.where('name LIKE ?', "%#{search_term}%")
+    elsif Rails.env.production?
+      @products = Product.where('name ilike ?', "%#{search_term}%")
+    end
+  end
+
 end
+# I'm on lesson 5.2 about 3/5 down the lesson about to start the section called "Product Form"
