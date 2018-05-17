26c4fa6e0c69e01b3ddb990da4e70e7943fa86f8
Bill Horst <bill@billhorst.com>
2018-05-16 22:04:06 -0600

    added email stuff

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -13,4 +13,12 @@ class SimplePagesController < ApplicationController
 
   def contact
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email, to: 'bill@billhorst.com', subject: "New message from #{@name}", body: @message).deliver_now
+  end
+
 end
